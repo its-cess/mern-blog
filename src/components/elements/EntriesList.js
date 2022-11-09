b6a243a34c1b9c1b80e2e0aaa432b5072d25be9d
Cecily Toro <torocecily@gmail.com>
2022-11-08 20:07:16 -0500

    delete entry functionality

--- src/components/elements/EntriesList.js
+++ src/components/elements/EntriesList.js
@@ -1,13 +1,20 @@
 import EntryItem from "./EntryItem";
 
 const EntriesList = (props) => {
+
   return (
     <ul>
       {props.entries.map((entry) => (
-        <EntryItem key={entry.id} id={entry.id} title={entry.title} body={entry.body} onDelete={props.onDeleteEntry}/>
+        <EntryItem
+          key={entry.id}
+          id={entry.id}
+          title={entry.title}
+          body={entry.body}
+          onDelete={props.onDeleteEntry}
+        />
       ))}
     </ul>
-  )
-}
+  );
+};
 
 export default EntriesList;
--- src/components/elements/EntryItem.js
+++ src/components/elements/EntryItem.js
@@ -1,14 +1,34 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
+import { AuthContext } from "../../context/auth-context";
 
 const EntryItem = (props) => {
+  const auth = useContext(AuthContext);
+
+  const onDeleteHandler = async () => {
+    const postId = props.id;
+
+    try {
+      await fetch(`http://localhost:4000/posts/${postId}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: "Bearer " + auth.token 
+        }
+      })
+      alert("post deleted!")
+      props.onDelete(props.id)
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <Fragment>
-    <li>
+    <li id={props.id}>
       <h2>{props.title}</h2>
       <h3>{props.body}</h3>
     </li>
     <button>Edit</button>
-    <button>Delete</button>
+    <button onClick={onDeleteHandler}>Delete</button>
     </Fragment>
   )
 }
