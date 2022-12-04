d3eb3f583c4125cb0c5eed3e2c247c617ed534bf
Cecily Toro <torocecily@gmail.com>
2022-12-04 14:05:17 -0500

    add options to select in edit profile

--- src/components/pages/EditProfile.js
+++ src/components/pages/EditProfile.js
@@ -1,28 +1,49 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 const EditProfile = () => {
+  const [birthdayMonth, setBirthdayMonth] = useState();
+  const [birthdayDay, setBirthdayDay] = useState();
+
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  const days = []
+
+  for (let i = 1; i < 32; i++) {
+    days.push(<option key={i} value={birthdayDay} name={birthdayDay}>{i}</option>)
+  }
+
+  const onMonthChangeHandler = (event) => {
+   setBirthdayMonth(event.target.value);
+  }
+
+  const onDayChangeHandler = (event) => {
+    setBirthdayDay(event.target.value);
+   }
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    console.log(birthdayMonth, birthdayDay);
+    alert("birthday updated")
+  }
+
   return (
     <Fragment>
       <h1>Edit Profile</h1>
-      <form>
-        <label>About me:</label>
+      <form onSubmit={onSubmitHandler}>
+        <label>Bio:</label>
         <textarea></textarea>
         <h4>Birthday:</h4>
         <label htmlFor="birthdayMonth">Month</label>
-          <select id="birthdayMonth">
+          <select id="birthdayMonth" onChange={onMonthChangeHandler}>
             <option default>Choose One</option>
-            <option value="January">January</option>
-            <option value="February">February</option>
+            {months.map((month) => <option key={month} value={birthdayMonth} name={month}>{month}</option>)}
           </select>
 
         <label htmlFor="birthdayDay">Day</label>
-          <select id="birthdayDay">
+          <select id="birthdayDay" onChange={onDayChangeHandler}>
             <option default>Choose One</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
+            {days}
           </select>
 
-        <h4>Member Since:</h4>
         <button type="submit">Update Profile</button>
       </form>
     </Fragment>
